ee9d59c1bc9aa1eef736f34610d458e78feaf4d9
Matt Travi <programmer@travi.org>
2020-06-23 15:55:14 -0500

    feat(unit-testing): enabled choosing from the provided frameworks
    rather than always choosing to scaffold mocha

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export {default as scaffoldUnitTesting} from './testing/unit';
 export {default as scaffoldChoice} from './choice-scaffolder';
-export {unitTesting as unitTestFrameworksSchema} from './testing/options-schemas';
+export {unitTestFrameworksSchema} from './testing/options-schemas';
