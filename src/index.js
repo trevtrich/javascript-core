40d6bf92acbbc0a9780485bc83a3107bf59be664
Matt Travi <programmer@travi.org>
2020-06-27 18:22:41 -0500

    feat(question-names): exposed the constants that define the names
    BREAKING CHANGE: retroactively marking this as breaking since i missed marking the commit that added
    the required `unitTestFrameworks` for the unit-testing scaffolder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as scaffoldUnitTesting} from './testing/unit';
 export {default as scaffoldChoice} from './choice-scaffolder';
 export {unitTestFrameworksSchema} from './testing/options-schemas';
+export {questionNames} from './prompts/question-names';
