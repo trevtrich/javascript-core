d75e0ef180b49ca851efcfdf5be55588cd21f54a
Matt Travi <programmer@travi.org>
2020-08-08 13:52:04 -0500

    feat(dependencies): exposed types constants

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export {default as scaffoldUnitTesting} from './testing/unit';
 export {default as scaffoldChoice} from './choice-scaffolder';
 export {unitTestFrameworksSchema} from './testing/options-schemas';
 export {questionNames} from './prompts/question-names';
+export {DEV_DEPENDENCY_TYPE, PROD_DEPENDENCY_TYPE} from './dependencies/types';
